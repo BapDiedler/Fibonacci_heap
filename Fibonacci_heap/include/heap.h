29b27c7af59fc11204e8e3a52c34cdf61cded391
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2024-03-31 17:41:18 +0200

    ajout de nouveaux éléments pour le tas

--- Fibonacci_heap/include/heap.h
+++ Fibonacci_heap/include/heap.h
@@ -3,23 +3,35 @@
 */
 #include <stdlib.h>
 
+//structure de la data
+typedef struct data{
+    int value;
+    int color;
+}* data;
+
 //structure du tas
 typedef struct node
 {
-    int data;
-    heap parent;
-    heap* child;
-}* heap, node;
+    data* data;
+    int nb_child;
+}* heap ,node;
 
-
+//création d'une data
+void init_data(data* d, int value);
 //initialisation de tas
 void init_heap(heap* h);
 //ajout d'une valeur dans un tas
 void add_heap(heap h, int d);
+//renvoie la position du père dans l'arbre
+int parent(heap h, int pos);
+//fusion de deux tas
+void fusion_heap(heap h1, heap h2);
 //supprimer un noeud du tas
 void delt_heap(heap h, int d);
 //supprimer min du tas
 void delt_min_heap(heap h);
+//affiche le min
+int min_heap(heap h);
 //afficher tas
 void prt_heap(heap h);
 //libérer la mémoire du tas
---
+++ Fibonacci_heap/src/heap.c
@@ -0,0 +1,43 @@
+/**
+ * implémentation des tas
+*/
+
+#include "include/heap.h"
+
+void init_heap(heap * h){
+    *h = (heap)malloc(sizeof(node));
+    (*h)->data = NULL;
+    (*h)->nb_child = 0;
+}
+
+void init_data(data* d, int value){
+    *d = (data)malloc(sizeof(struct data));
+    (*d)->value = value;
+    (*d)->color = 0; 
+}
+
+void go_up(heap h, int pos_child, int pos_parent){
+    data tmp = h->data[pos_child];
+    h->data[pos_child] = h->data[pos_parent];
+    h->data[pos_parent] = tmp;
+}
+
+void add_heap(heap h, int d){
+    //voir la théorie
+    data new_d;
+    init_data(&new_d, d);
+    h->data[h->nb_child-1] = new_d;
+    h->nb_child++;
+    int pos = h->nb_child-1;
+    int pos_parent;
+    while(1){
+        pos_parent = parent(h, pos);
+        if(h->data[pos] >= pos_parent)
+            break;
+        go_up(h, pos, pos_parent);
+    }
+}
+
+int parent(heap h, int pos){
+
+}
